c5476c2d36d78f5555ed1d2cb92d09badf415719
digDev <rodrigo.erjota@gmail.com>
2025-10-19 06:50:33 -0300

    Init files and post models simple

---
+++ blog_project/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('created_at', 'author')
+    ordering = ('-created_at',)
+
+# Register your models here.
